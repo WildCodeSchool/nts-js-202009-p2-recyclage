fb46f292a3e665c6e1e2ed7866d05d0ac3bccd69
bryce-mez <brice.mezerette.pro@gmail.com>
2020-10-29 15:34:08 +0100

    add feature filter

---
+++ src/components/FilterList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Filter from './Filter';
+
+const typeFilter = [
+  {
+    filterTitle: 'Filtrer par déchets',
+    filter: [
+      'Verre',
+      'Carton',
+      'Plastique',
+      'Electroniques',
+      'Déchets dangereux',
+      'Encombrants',
+      'Alimentaires',
+    ],
+    id: 1,
+  },
+  {
+    filterTitle: 'Filtrer par type de collecte',
+    filter: [
+      'Compost',
+      'Colonnes enterrées',
+      'Colonnes aériennes',
+      'Déchetterie',
+    ],
+    id: 2,
+  },
+  {
+    filterTitle: 'Lieux et Rdv Tri-sac',
+    filter: ['Tri-sac'],
+    id: 3,
+  }
+]
+
+const FilterList = () => (
+  <div>
+  { typeFilter.map((typeFilter) => (
+    <Filter {...typeFilter}  key={typeFilter.id}/>))}
+ </div>
+ );
+
+
+export default FilterList;
