122b85d7013fccdcb724de2e4de05e87b3eaef1a
Maxime-Monjal <maxime.monjal@gmail.com>
2020-11-25 13:49:17 +0100

    css

--- src/components/MissionBoxContainer.jsx
+++ src/components/MissionBoxContainer.jsx
@@ -11,50 +11,35 @@ const missions = [
     consignes:
       "Recycle tes bouteilles en plastique, jette-les dans un Tri'Sac jaune ou dans un point d'apport plastique.",
     points: 20,
-<<<<<<< HEAD
-=======
-    jauge : 0.2,
->>>>>>> 1fb0dc548e74bd739f0a00e265f51a212f8b76c1
+    jauge: 0.2,
   },
   {
     id: 2,
     consignes:
       'Recycle tes bouteilles et pots en verre, jette-les dans un conteneur dédié près de chez toi. ',
     points: 30,
-<<<<<<< HEAD
-=======
-    jauge : 0.3,
->>>>>>> 1fb0dc548e74bd739f0a00e265f51a212f8b76c1
+    jauge: 0.3,
   },
   {
     id: 3,
     consignes:
       'Remplace tes achats sous emballages plastique/carton par des produits vendu en vrac. ',
     points: 20,
-<<<<<<< HEAD
-=======
-    jauge : 0.2,
->>>>>>> 1fb0dc548e74bd739f0a00e265f51a212f8b76c1
+    jauge: 0.2,
   },
   {
     id: 4,
     consignes:
       'Trie tes déchets électroniques, ampoules, piles,... en déchetterie ',
     points: 10,
-<<<<<<< HEAD
-=======
-    jauge : 0.1,
->>>>>>> 1fb0dc548e74bd739f0a00e265f51a212f8b76c1
+    jauge: 0.1,
   },
   {
     id: 5,
     consignes:
       'Réduis le volume de ta poubelle en compostant tes déchets alimentaires. ',
     points: 30,
-<<<<<<< HEAD
-=======
-    jauge : 0.3,
->>>>>>> 1fb0dc548e74bd739f0a00e265f51a212f8b76c1
+    jauge: 0.3,
   },
 ];
 
@@ -63,7 +48,7 @@ class MissionBoxContainer extends React.Component {
     super(props);
     this.state = {
       index: 0,
-      };
+    };
     this.changeBar = this.changeBar.bind(this);
     this.indexIncrement = this.indexIncrement.bind(this);
     this.indexDecrement = this.indexDecrement.bind(this);
@@ -72,8 +57,7 @@ class MissionBoxContainer extends React.Component {
   changeBar(jauge) {
     const { barChanged } = this.props;
     barChanged(jauge);
-  };
-
+  }
 
   indexIncrement() {
     if (this.state.index < missions.length - 1) {
@@ -106,11 +90,7 @@ class MissionBoxContainer extends React.Component {
             <Points mission={mission} />
           </div>
           <MissionBox mission={mission} />
-<<<<<<< HEAD
-          <Button addPoint={this.addPoint} mission={mission} />
-=======
-          <Button mission={mission} changeBar={this.changeBar}/>
->>>>>>> 1fb0dc548e74bd739f0a00e265f51a212f8b76c1
+          <Button mission={mission} changeBar={this.changeBar} />
         </div>
         <div onClick={this.indexIncrement} className="arrows">
           <Arrow direction="right" />
