a71ec4e30ce972509a1b4affd30b9f26c3364377
Maxime-Monjal <maxime.monjal@gmail.com>
2020-11-26 16:35:42 +0100

    fix err

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Header from './Header';
-import Avatar2 from './Avatar2';
+import Avatar from './Avatar';
 import ScoreAvatar from './ScoreAvatar';
 import Footer from './Footer';
 
 function Profil(props) {
-
   return (
     <div className="profil">
       <Header />
-      <Avatar2 />
-      <ScoreAvatar jauge= {props.jauge}/>
+      <Avatar />
+      <ScoreAvatar jauge={props.jauge} />
       <Footer />
     </div>
   );
